a205734f7f5f97955080810d7a62219fa4f94112
alp133 <102556053+alp133@users.noreply.github.com>
2022-12-05 03:22:04 -0500

    Player movement implemented

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import EnemyController from "./EnemyController.js";
+import Player from "./Player.js";
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -12,10 +13,12 @@ TO CHANGE IMAGE CHANGE HERE
 background.src = 'assets/images/space.png'; 
 
 const enemyController = new EnemyController(canvas);
+const player = new Player(canvas, 3);
 
 function game(){
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     enemyController.draw(ctx);
+    player.draw(ctx);
 }
 
 setInterval(game,1000/60);
